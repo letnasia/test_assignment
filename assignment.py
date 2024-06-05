9e5c3ae15fae08ddde873c7c1e4d4631abfc559d
Anastasiia Letnianko <140107367+letnasia@users.noreply.github.com>
2024-06-05 16:47:45 +0300

    Interview changes

--- assignment.py
+++ assignment.py
@@ -15,47 +15,17 @@ class Shape:
     def parse(self, parameters: List[str]) -> bool:
         raise NotImplemented()
 
+    def parse_point(self, i: int, lst: List[str]):
+        x = self._parse_int(lst[i + 1])
+        y = self._parse_int(lst[i + 2])
+        return x, y
+
     @staticmethod
     def _parse_int(text: str):
         try:
             return int(text)
         except:
             return None
-
-
-class Square(Shape):
-    top = None
-    right = None
-    side = None
-
-    def get_area(self):
-        return self.side * self.side
-
-    def get_perimeter(self):
-        return self.side * 4
-
-    def name(self) -> str:
-        return "Square"
-
-    def parse(self, parameters: List[str]) -> bool:
-        i = 0
-        while i < len(parameters):
-            if parameters[i] == "TopRight":
-                if i + 2 >= len(parameters):
-                    return False
-                self.top = self._parse_int(parameters[i + 1])
-                self.right = self._parse_int(parameters[i + 2])
-                i += 3
-            elif parameters[i] == "Side":
-                if i + 1 >= len(parameters):
-                    return False
-                self.side = self._parse_int(parameters[i + 1])
-                i += 2
-            else:
-                return False
-        return (self.top is not None
-                and self.right is not None
-                and self.side is not None)
 
 
 class Rectangle(Shape):
@@ -77,20 +47,23 @@ class Rectangle(Shape):
     def name(self) -> str:
         return "Rectangle"
 
+    def parse_point(self, i: int, lst: List[str]):
+        x = self._parse_int(lst[i + 1])
+        y = self._parse_int(lst[i + 2])
+        return x, y
+
     def parse(self, parameters: List[str]) -> bool:
         i = 0
         while i < len(parameters):
             if parameters[i] == "TopRight":
                 if i + 2 >= len(parameters):
                     return False
-                self.top = self._parse_int(parameters[i + 1])
-                self.right = self._parse_int(parameters[i + 2])
+                self.top, self.right = self.parse_point(i, parameters)
                 i += 3
             elif parameters[i] == "BottomLeft":
                 if i + 2 >= len(parameters):
                     return False
-                self.bottom = self._parse_int(parameters[i + 1])
-                self.left = self._parse_int(parameters[i + 2])
+                self.bottom, self.left = self.parse_point(i, parameters)
                 i += 3
             else:
                 return False
@@ -100,6 +73,36 @@ class Rectangle(Shape):
                 or self.bottom is None):
             return False
         return self.bottom <= self.top and self.left <= self.right
+
+
+class Square(Rectangle):
+    side = None
+
+    def name(self) -> str:
+        return "Square"
+
+    def parse(self, parameters: List[str]) -> bool:
+        i = 0
+        while i < len(parameters):
+            if parameters[i] == "TopRight":
+                if i + 2 >= len(parameters):
+                    return False
+                self.top, self.right = self.parse_point(i, parameters)
+                i += 3
+            elif parameters[i] == "Side":
+                if i + 1 >= len(parameters):
+                    return False
+                self.side = self._parse_int(parameters[i + 1])
+                i += 2
+            else:
+                return False
+        is_valid = (self.top is not None
+                    and self.right is not None
+                    and self.side is not None)
+        if is_valid:
+            self.bottom = self.top - self.side
+            self.left = self.right - self.side
+        return is_valid
 
 
 class Circle(Shape):
@@ -175,7 +178,7 @@ def test():
         s = cls()
         if s.parse(params):
             return s
-    
+
     assert test_shape_parse(Square, "TopRight 1 1 Side 1"), "Square: Valid"
     assert test_shape_parse(Square, "Side 1 TopRight 1 1"), "Square: Valid"
     assert not test_shape_parse(Square, "TopRight 1 1"), "Square: Invalid"
@@ -184,13 +187,24 @@ def test():
     assert not test_shape_parse(Square, "Side 1 TopRight 1"), "Square: Invalid"
     assert not test_shape_parse(Square, "Side 1 TopRight 1 1 Foo 3"), "Square: Invalid"
     assert not test_shape_parse(Square, "1 1 1"), "Square: Invalid"
+    assert not test_shape_parse(Square, ""), "Square: Invalid"
     shape = test_shape_parse(Square, "TopRight 1 1 Side 1")
     assert shape.get_perimeter() == 4
     assert shape.get_area() == 1
+    assert shape.top == 1
+    assert shape.right == 1
+    assert shape.bottom == 0
+    assert shape.left == 0
+    assert shape.side == 1
     shape = test_shape_parse(Square, "TopRight 2 2 Side 2")
+    assert shape.top == 2
+    assert shape.right == 2
+    assert shape.bottom == 0
+    assert shape.left == 0
+    assert shape.side == 2
     assert shape.get_perimeter() == 8
     assert shape.get_area() == 4
-    
+
     assert test_shape_parse(Rectangle, "TopRight 2 2 BottomLeft 1 1"), "Rectangle: Valid"
     assert test_shape_parse(Rectangle, "BottomLeft 1 1 TopRight 2 2"), "Rectangle: Valid"
     assert not test_shape_parse(Rectangle, "TopRight 1 1"), "Rectangle: Invalid"
@@ -206,7 +220,7 @@ def test():
     shape = test_shape_parse(Rectangle, "TopRight 4 4 BottomLeft 1 1")
     assert shape.get_perimeter() == 12
     assert shape.get_area() == 9
-    
+
     assert test_shape_parse(Circle, "Center 1 1 Radius 2"), "Circle: Valid"
     assert test_shape_parse(Circle, "Radius 2 Center 1 1"), "Circle: Valid"
     assert not test_shape_parse(Circle, "Center 1 1"), "Circle: Invalid"
@@ -225,4 +239,5 @@ def test():
 
 if __name__ == "__main__":
     test()
-    main()
+    # main()
+
